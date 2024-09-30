fee432ef5a1c1c2c65d2f933204e1b9351c72649
Gautier29 <gautier.evennou@gmail.com>
2024-09-30 09:58:35 +0200

    Hide-R watermark xp script

--- watermark.sh
+++ watermark.sh
@@ -1,4 +1,4 @@
-source /home/gevennou/ip2p/bin/activate #USE MODIFED TRANSFORMERS PACKAGE
+source /home/gevennou/swift_release/bin/activate 
 
-python /home/gevennou/swift/watermark_dir.py -L facebook/opt-125m --watermark_power 1.0 --modulation cyclic --adapter_path models/finetuned_llm/ \
+python watermark_dir.py -L facebook/opt-125m --watermark_power 1.0 --modulation cyclic --adapter_path models/finetuned_llm/ \
     --watermark_encoder_model models/Hide-R/bzhenc.pth --watermark_decoder_model models/Hide-R/bzhdec.pth
